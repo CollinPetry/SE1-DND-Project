72fbb159ed5dd014405a1971e7476f5ef8006ba5
Collin Petry <52768589+CollinPetry@users.noreply.github.com>
2022-05-04 18:22:36 -0500

    Working and update diceroller

---
+++ static/diceroller.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+    $('form').on('submit', function(event) {
+      $.ajax({
+         data : {
+            d4 : $('#d4').val(),
+            d6 : $('#d6').val(),
+            d8 : $('#d8').val(),
+            d10 : $('#d10').val(),
+            d12 : $('#d12').val(),
+            d20 : $('#d20').val(),
+            d100 : $('#d100').val(),
+                },
+            type : 'POST',
+            url : '/diceroller'
+           })
+       .done(function(data) {
+        $('#roll').text(data['roll']).show();
+     });
+     event.preventDefault();
+     });
+});
