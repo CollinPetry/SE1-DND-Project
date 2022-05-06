9f95ec11f2cb0be5b55af2adfa666cf4797cfaaf
Collin Petry <52768589+CollinPetry@users.noreply.github.com>
2022-05-06 17:30:39 -0500

    Revert "Added Campaign Table"
    This reverts commit 6c1bbe19

--- databases/user_accounts.sql
+++ databases/user_accounts.sql
@@ -5,20 +5,3 @@ create table IF NOT EXISTS users(
     password varchar(50),
     primary key (id)
 );
-
-create table IF NOT EXISTS campaign(
-	id int(11) NOT NULL AUTO_INCREMENT,
-    name varchar(50) Not Null,
-	players int(10),
-    role varchar(50),
-    primary key (id)
-);
-
-CREATE TABLE IF NOT EXISTS usercampaign(
-    id int(11) NOT NULL AUTO_INCREMENT,
-    userid int(11) NOT NULL,
-    campaignid int(11) NOT NULL,
-    PRIMARY KEY(id),
-    FOREIGN KEY(userid) REFERENCES users(id),
-    FOREIGN KEY(campaignid) REFERENCES campaign(id)
-    )
