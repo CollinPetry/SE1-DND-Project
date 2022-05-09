0271564813848fa5926289dc5bf5228683652a6f
seaweedjay <aloisiusjgrzybowski@gmail.com>
2022-05-09 12:01:14 -0500

    character builder fixed!

--- static/script.js
+++ static/script.js
@@ -14,20 +14,4 @@ $(document).ready(function() {
      });
 });
 
-function myFunction(a) {
-  a.parentNode.getElementsByClassName("dropdown-content")[0].classList.toggle("show");
-}
-
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
 }
